c3ec05a823033c94069d208f651b3d172d0f73c9
“Tyoma” <seklertyoma@gmail.com>
2024-02-10 12:20:08 +0300

    fix: button styles

--- apps/web/src/theme/components/Button/index.ts
+++ apps/web/src/theme/components/Button/index.ts
@@ -5,12 +5,12 @@ import classes from './index.module.css';
 export default Button.extend({
   defaultProps: {
     size: 'md',
-    bg: '#2B77EB',
     radius: 8,
     px: 20,
     py: 4,
   },
   classNames: {
     label: classes.label,
+    root: classes.root,
   },
 });
